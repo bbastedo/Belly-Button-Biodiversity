abc70719c6327351bf2120a9acd557e381a7d05f
Ben Bastedo <bbastedo@comcast.net>
2020-02-04 19:34:44 -0600

    Commented code

--- static/app.js
+++ static/app.js
@@ -2,13 +2,17 @@
 // Ben Bastedo 
 
 //1. Use the D3 library to read in `samples.json`
+//2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 function barChart (samples){
     d3.json("samples.json").then((data)=>{
         var samples = data.samples;
         var dataArray = samples.filter(sampleObj => sampleObj.id == sample);
         var results = dataArray[0];
+       // Use `otu_ids` as the labels for the bar chart.
         var otu_ids = result.otu_ids;
+        // Use `otu_labels` as the hovertext for the chart
         var otu_labels = result.otu_labels;
+        // Use `sample_values` as the values for the bar chart.
         var sample_values = results.sample_values;
         console.log(otu_ids);
         
@@ -32,3 +36,5 @@ function barChart (samples){
 
     })
 }
+
+//
